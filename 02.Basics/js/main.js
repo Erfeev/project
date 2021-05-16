0de0f1cb982e84e43674b7dd24cfcf8a52f2dcad
Bohdan <anyway2839@gmail.com>
2021-05-16 14:57:56 +0300

    first commit

---
+++ 02.Basics/js/main.js
@@ -0,0 +1,80 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// {
+//     let result = 50;
+// }
+// console.log(result);
+
+
+// !!  Числа
+// let number = 4.6;
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+// const age = '55';
+
+// const bool = true;
+// const Bool = false;
+
+// console.log(something); //null
+
+// let und;
+// console.log(und);
+
+
+// // обьекты - objects
+// const obj = {
+//     name: 'Bohdan',
+//     age: 22,
+//     isMarried: false
+// };
+// console.log(obj.name);
+// console.log(obj['age']);
+
+// // масивы - arrays
+// let array = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, [], true];
+// console.log(array[1], array[2]);
+// alert('Hello');
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = prompt('Are you drunk?', ' ');
+// console.log( typeof(answer) );
+// const answer = prompt('Your age?', '10');
+// console.log(answer + 5); //string + string = 105
+// const Answer = +prompt('Your age?', '10');
+// console.log(Answer + 5); //number + number = 15
+// const answers = [];
+// // answers[0] = prompt('name', '');
+// // answers[1] = prompt('second name', '');
+// // answers[2] = prompt('age', '');
+// // document.write(answers);
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+// const user = prompt('enter your name here');
+// alert(`hello, ${user}! How are you, ${user}?`);
+
+//  //операторы
+// let incr = 10,
+//     decr = 10;
+// // ++incr;
+// // --decr;
+// console.log(++incr);
+// console.log(--decr); 
+// const isChecked = true,
+//       isClosed = false;
+// console.log(isChecked || isClosed);
+// console.log(isChecked && isClosed);
